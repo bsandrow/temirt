8f1c4d9e3fe9ba16e0cf25eab291edfed6350ac4
Brandon Sandrowicz <brandon@sandrowicz.org>
2011-12-04 16:04:29 -0800

    Breaking out the monolithic result class

--- pmet/__init__.py
+++ pmet/__init__.py
@@ -155,7 +155,35 @@ class TrimetResult(dict):
         self['locations']      = [ TrimetLocation(location) for location in tree.xpath("./*[local-name()='location']") ]
         self['arrivals']       = [ TrimetArrival(arrival) for arrival in tree.xpath("./*[local-name()='arrival']") ]
         self['route_statuses'] = [ TrimetRouteStatus(route_status) for route_status in tree.xpath("./*[local-name()='routeStatus']") ]
-        self['detours']        = [ Detour(detour) for detour in tree.xpath("./*[local-name()='detour']") ]
+
+class BaseResult(dict):
+    """The base class for handling <resultSet> responses from the Trimet API
+    service. Subclasses can define the _process_result_xml() method to hook
+    into initialization processing, and get passed the root ElementTree."""
+
+    def __init__(self, content):
+        """Initialize the result. If there is an <errorMessage> element, then
+        don't bother to do anymore processing. """
+        dict.__init__(self)
+        tree = etree.fromstring(content)
+        self._get_error_message(tree)
+        if 'error_message' not in self:
+            self._process_result_xml(tree)
+
+    def _get_error_message(self, tree):
+        error_messages = tree.xpath("./*[local-name()='errorMessage']")
+        if error_messages:
+            if len(error_messages) > 1:
+                raise TrimetParseError("Error: Too many <errorMessage> elements")
+            else:
+                self['error_message'] = error_messages[0].text
+
+    def _process_result_xml(self, tree):
+        pass
+
+class DetoursResult(BaseResult):
+    def _process_result_xml(self, tree):
+        self['detours'] = [ Detour(detour) for detour in tree.xpath("./*[local-name()='detour']") ]
 
 class Route(object):
     def __init__(self, element):
@@ -228,7 +256,7 @@ class TrimetApi(object):
 
         response = requests.get(url)
         if response.status_code == 200:
-            return TrimetResult(response.content)
+            return DetoursResult(response.content)
         else:
             raise TrimetHTTPError("HTTP Code: %d" % (response.status_code))
 
