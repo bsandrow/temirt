1c5ccb7cc799e58bfc0bb305cea3d1a1ca9df236
Brandon Sandrowicz <brandon@sandrowicz.org>
2011-12-07 19:02:32 -0800

    Moving from class-based api access to module-based

--- pmet/__init__.py
+++ pmet/__init__.py
@@ -1,69 +1,11 @@
-import datetime
-import sys
-
-import requests
-
-import pmet.results
-from pmet.errors import PmetHTTPError, PmetParseError
-
-base_urls = {
-    'arrivals': 'http://developer.trimet.org/ws/V1/arrivals',
-    'detours' : 'http://developer.trimet.org/ws/V1/detours',
-}
-
-class Api(object):
-    application_id = None
-
-    def __init__(self, appid):
-        self.application_id = appid
-
-    def _build_url(self, key, params):
-        params['appID'] = self.application_id
-
-        query_params = []
-        for k,v in params.iteritems():
-            if type(v) == list:
-                query_params += [ (k,value) for value in v ]
-            else:
-                query_params.append((k,v))
-
-        query_string = '&'.join([ '='.join(kvpair) for kvpair in query_params ])
-
-        return "%s?%s" % (base_urls[key], query_string)
-
-    def arrivals(self, location_ids):
-        params = { 'locIDs': ','.join(str(locid) for locid in location_ids) }
-        url = self._build_url('arrivals', params)
-
-        response = requests.get(url)
-        if response.status_code == 200:
-            return pmet.results.ArrivalsResult(response.content)
-        else:
-            raise PmetHTTPError("HTTP Code: %d" % (response.status_code))
-
-    def detours(self, routes):
-        params = { 'routes': ','.join(str(route) for route in routes) }
-        url = self._build_url('detours', params)
-
-        response = requests.get(url)
-        if response.status_code == 200:
-            return pmet.results.DetoursResult(response.content)
-        else:
-            raise PmetHTTPError("HTTP Code: %d" % (response.status_code))
-
+import pmet.api as api
 
 if __name__ == '__main__':
     import os
-    API_KEY = os.environ['TRIMET_API_KEY']
+    api.application_id = os.environ['TRIMET_API_KEY']
 
-    # api = Api(API_KEY)
     # from pprint import pprint as PP
     # PP(api.arrivals(['6849']))
 
-    # with open('test-arrival.xml') as xmlfh:
-    #     from pprint import pprint as PP
-    #     PP(TrimetResult(xmlfh.read()))
-
-    api = Api(API_KEY)
     from pprint import pprint as PP
     PP(api.detours(['44']))
---
+++ pmet/api.py
@@ -0,0 +1,48 @@
+import datetime
+import sys
+
+import requests
+
+import pmet.results
+from pmet.errors import PmetHTTPError, PmetParseError
+
+application_id = None
+base_urls      = {
+    'arrivals': 'http://developer.trimet.org/ws/V1/arrivals',
+    'detours' : 'http://developer.trimet.org/ws/V1/detours',
+}
+
+def _build_url(key, params):
+    params['appID'] = application_id
+
+    query_params = []
+    for k,v in params.iteritems():
+        if type(v) == list:
+            query_params += [ (k,value) for value in v ]
+        else:
+            query_params.append((k,v))
+
+    query_string = '&'.join([ '='.join(kvpair) for kvpair in query_params ])
+
+    return "%s?%s" % (base_urls[key], query_string)
+
+def arrivals(location_ids):
+    params = { 'locIDs': ','.join(str(locid) for locid in location_ids) }
+    url = _build_url('arrivals', params)
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        return pmet.results.ArrivalsResult(response.content)
+    else:
+        raise PmetHTTPError("HTTP Code: %d" % (response.status_code))
+
+def detours(routes):
+    params = { 'routes': ','.join(str(route) for route in routes) }
+    url = _build_url('detours', params)
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        return pmet.results.DetoursResult(response.content)
+    else:
+        raise PmetHTTPError("HTTP Code: %d" % (response.status_code))
+
