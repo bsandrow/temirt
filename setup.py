7a659c4cea1d288ddf9005f94092662a0b88d0b5
Brandon Sandrowicz <brandon@sandrowicz.org>
2011-12-09 19:57:08 -0800

    Adding a setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    print "Falling back to distutils. Functionality may be limited."
+    from distutils.core import setup
+
+config = {
+    'description'       : 'A Python interface to the Trimet Web API',
+    'author'            : 'Brandon Sandrowicz',
+    'url'               : 'http://github.com/bsandrow/pmet',
+    'author_email'      : 'brandon@sandrowicz.org',
+    'version'           : 0.1,
+    'install_requires'  : [],
+    'packages'          : ['pmet'],
+    'name'              : 'Pmet',
+}
+
+setup(**config)
